6c8e1673adedb5d76ef561fa447b78df9637f646
Dmitry Ludvik <ludvikevichdmitry@gmail.com>
2023-03-20 00:18:18 +0300

    Add main window and cadet page

---
+++ project/CadetAccounting/CadetAccounting/Pages/CadetPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CadetAccounting.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CadetPage.xaml
+    /// </summary>
+    public partial class CadetPage : Page
+    {
+        public CadetPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
