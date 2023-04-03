047c8c7b0002997bacd54202359c1c93ba80abda
Dmitry Ludvik <ludvikevichdmitry@gmail.com>
2023-04-03 18:22:16 +0300

    Add contact folder selection

--- project/CadetAccounting/CadetAccounting/WordHelper.cs
+++ project/CadetAccounting/CadetAccounting/WordHelper.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using CadetAccounting.DBModel;
 using Spire.Doc;
 using System.IO;
+using System.Security.Principal;
+using System.Windows;
+using System.Windows.Forms;
 
 namespace CadetAccounting
 {
@@ -24,8 +27,50 @@ namespace CadetAccounting
             document.Replace("<dateStart>", contract.Cadet.Group.DateStart.ToString("dd/MMMM/yyyy"), false, true);
             document.Replace("<price>", contract.TotalPrice.ToString(), false, true);
 
-            document.SaveToFile(@"C:\Users\Asus\Desktop\contract.docx", FileFormat.Docx);
-            System.Diagnostics.Process.Start(@"C:\Users\Asus\Desktop\contract.docx");
+            try
+            {
+                string link;
+
+                using (StreamReader reader = new StreamReader(Directory.GetCurrentDirectory() + @"\data.txt"))
+                {
+                    link = reader.ReadLine();
+                }
+
+                if (!string.IsNullOrEmpty(link) && !Directory.Exists(link))
+                {
+                    link = null;
+                    using (StreamWriter writer = new StreamWriter(Directory.GetCurrentDirectory() + @"\data.txt"))
+                    {
+                        writer.Flush();
+                    }
+                }
+                if (string.IsNullOrEmpty(link))
+                {
+                    FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+                    DialogResult result = folderBrowser.ShowDialog();
+                    if (!string.IsNullOrWhiteSpace(folderBrowser.SelectedPath))
+                    {
+                        using (StreamWriter writer = new StreamWriter(Directory.GetCurrentDirectory() + @"\data.txt"))
+                        {
+                            writer.WriteLine(folderBrowser.SelectedPath);
+                            link = folderBrowser.SelectedPath;
+                        }
+                    }
+                    else
+                    {
+                        System.Windows.MessageBox.Show("Выберите папку для сохранение файлов", "Внимание",MessageBoxButton.OK, MessageBoxImage.Warning);
+                        return;
+                    }
+                }
+
+                string newFilePath = link + $"\\{contract.DateOfConclusion.Day}_{contract.DateOfConclusion.Month}_{contract.DateOfConclusion.Year}_{contract.Cadet.Surname}_{contract.Cadet.Name}_{contract.Cadet.Patronymic}.docx";
+                document.SaveToFile(newFilePath, FileFormat.Docx);
+                System.Diagnostics.Process.Start(newFilePath);
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }
